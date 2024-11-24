6119a7c3f961f4ae8a560549820d697816bf9318
JonathanKarin <69160570+JonathanKarin@users.noreply.github.com>
2024-11-24 17:42:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# List of dependencies
 requirements = [
     "numpy",
     "scanpy",
@@ -16,12 +21,19 @@ requirements = [
 setup(
     name="Annotatability",
     version="0.0.5",
-    packages=["Annotatability"],
+    packages=find_packages(),  # Automatically detect all packages
     url="https://github.com/nitzanlab/Annotatability",
     license="MIT License",
     author="jonathankarin",
     author_email="jonathan.karin@mail.huji.ac.il",
     description="Interpreting single cell data using annotation-trainability analysis",
-    python_requires=">=3",
-    install_requires=requirements
+    long_description=long_description,  # Correctly read from README.md
+    long_description_content_type="text/markdown",  # Specify content type
+    python_requires=">=3.7",  # Specify a specific Python version or range
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
